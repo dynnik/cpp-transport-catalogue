14e2afcc8cc48819a56a8dc7c0a000b2a19757b7
dynnik <110823273+dynnik@users.noreply.github.com>
2023-02-12 20:02:10 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,9 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+void Stat_Reader(catalogue::TransportCatalogue t);
+
+void PrintBusInfo(catalogue::BusRoute r);
+
+void PrintStopInfo(const catalogue::StopRoutes& s);
