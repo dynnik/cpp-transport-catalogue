a9b2d28319bff8de8dfc6c3f1bbf5b419ac11393
dynnik <110823273+dynnik@users.noreply.github.com>
2023-02-12 20:01:20 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,10 @@
-// место для вашего кода
+#include "geo.h"
+#include "input_reader.h"
+#include "transport_catalogue.h"
+#include "stat_reader.h"
+
+int main(){
+    catalogue::TransportCatalogue t(InputReader());
+    Stat_Reader(t);
+    return 0;
+}
