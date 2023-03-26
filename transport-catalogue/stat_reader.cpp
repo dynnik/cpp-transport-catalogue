be3b1cf10539d73623c9cf2d8541dfc94689b463
dynnik <110823273+dynnik@users.noreply.github.com>
2023-03-26 15:35:22 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -4,48 +4,48 @@
 using namespace std::string_literals;
 using namespace catalogue;
 
-void Stat_Reader(TransportCatalogue t){
+void Stat_Reader(std::istream& in, TransportCatalogue t) {
     int num;
-    std::cin >> num;
-    std::cin.ignore();
-    for(int i = 0; i < num; ++i){
+    in >> num;
+    in.ignore();
+    for (int i = 0; i < num; ++i) {
         std::string query;
-        std::getline(std::cin, query);
-        while (query[0] == ' '){
+        std::getline(in, query);
+        while (query[0] == ' ') {
             query.erase(0, 1);
         }
-        if(query.substr(0, 3) == "Bus"s){
+        if (query.substr(0, 3) == "Bus"s) {
             query.erase(0, 3);
             BusRoute r = t.RouteInformation(query);
-            PrintBusInfo(r);
+            PrintBusInfo(std::cout, r);
         }
-        if(query.substr(0, 4) == "Stop"s){
+        if (query.substr(0, 4) == "Stop"s) {
             query.erase(0, 4);
             StopRoutes s = t.StopInformation(query);
-            PrintStopInfo(s);
+            PrintStopInfo(std::cout, s);
         }
     }
 }
-void PrintBusInfo(BusRoute route){
-    if(!route.is_found){
-        std::cout << "Bus "s << route.bus_name << ": not found"s << std::endl;
+void PrintBusInfo(std::ostream& out, BusRoute& route) {
+    if (!route.is_found) {
+        out << "Bus "s << route.bus_name << ": not found"s << std::endl;
     }
-    else{
-        std::cout << "Bus "s << route.bus_name << ": " << route.stops << " stops on route, "s << route.unique_stops << " unique stops, "s << route.true_length << " route length, "s << route.curvature << " curvature"s<<std::endl;
+    else {
+        out << "Bus "s << route.bus_name << ": " << route.stops << " stops on route, "s << route.unique_stops << " unique stops, "s << route.true_length << " route length, "s << route.curvature << " curvature"s << std::endl;
     }
 }
 
-void PrintStopInfo(const StopRoutes& stop){
-    if(!stop.is_found){
-        std::cout << "Stop "s << stop.stop_name << ": not found"s << std::endl;
+void PrintStopInfo(std::ostream& out, const StopRoutes& stop) {
+    if (!stop.is_found) {
+        out << "Stop "s << stop.stop_name << ": not found"s << std::endl;
     }
-    else if(stop.is_found && stop.routes.empty()){
-        std::cout << "Stop "s << stop.stop_name << ": no buses"s << std::endl;
+    else if (stop.is_found && stop.routes.empty()) {
+        out << "Stop "s << stop.stop_name << ": no buses"s << std::endl;
     }
-    else{
-        std::cout << "Stop "s << stop.stop_name << ": buses"s;
-        for(auto i : stop.routes){
-            std::cout << " "s << i;
+    else {
+        out << "Stop "s << stop.stop_name << ": buses"s;
+        for (auto i : stop.routes) {
+            out << " "s << i;
         }
         std::cout << std::endl;
     }
