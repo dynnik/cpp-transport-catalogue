3728c2f6885eee9656471141f7efd5588bf998d9
dynnik <110823273+dynnik@users.noreply.github.com>
2023-03-26 15:33:38 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+const int EarthRad = 6371000;
 struct Coordinates {
     double lat;
     double lng;
@@ -20,6 +21,6 @@ inline double ComputeDistance(Coordinates from, Coordinates to) {
     }
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
+        + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * EarthRad;
 }
