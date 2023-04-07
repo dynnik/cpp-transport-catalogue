a440d47bf9a558bb13068999995789414f9e4b32
dynnik <110823273+dynnik@users.noreply.github.com>
2023-04-07 17:49:17 +0300

    Delete stat_reader.h

--- transport-catalogue/stat_reader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "transport_catalogue.h"
-
-void Stat_Reader(std::istream& in, catalogue::TransportCatalogue t);
-
-void PrintBusInfo(std::ostream& out, catalogue::BusRoute& r);
-
-void PrintStopInfo(std::ostream& out, const catalogue::StopRoutes& s);
